0b6a0018c4b87d75cf329676d1deaf59d3e4db09
alivekeeper <308771934@qq.com>
2020-01-06 16:14:07 +0800

    选择和插入排序

---
+++ src/main/java/com/tmx/stuckData/sortArithmetic/InsertSort.java
@@ -0,0 +1,98 @@
+package com.tmx.stuckData.sortArithmetic;
+
+import java.util.Arrays;
+
+/**
+ * @author mingxiong.tang
+ * @Date: 2020/1/6 15:38
+ */
+public class InsertSort {
+    /**
+     *1.思路
+     */
+    public static void main(String[] args) {
+
+        int[] arr={133,22,44,1};
+
+        int insertValue=arr[1];
+        int insertIndex=1-1; //插入数据的前一个数的下标
+
+        /**
+         * 1. insertIndex >=0 确保数组不越界
+         * 2. insertValue>arr[insertIndex] 确保未找到插入位置
+         * 3. indexIndex 移动
+         */
+        while (insertIndex >= 0 && insertValue < arr[insertIndex]){
+            arr[insertIndex+1]=arr[insertIndex];
+            insertIndex--;
+        }
+        arr[insertIndex+1]=insertValue;
+        System.out.println( "第"+(insertIndex+1)+"轮插入排序推导："+ Arrays.toString(arr) );
+
+        insertValue=arr[2];
+        insertIndex=2-1; //插入数据的前一个数的下标
+
+        /**
+         * 1. insertIndex >=0 确保数组不越界
+         * 2. insertValue>arr[insertIndex] 确保未找到插入位置
+         * 3. indexIndex 移动
+         */
+        while (insertIndex >= 0 && insertValue < arr[insertIndex]){
+            arr[insertIndex+1]=arr[insertIndex];
+            insertIndex--;
+        }
+        arr[insertIndex+1]=insertValue;
+        System.out.println( "第"+(insertIndex+1)+"轮插入排序推导："+ Arrays.toString(arr) );
+
+
+        insertValue=arr[3];
+        insertIndex=3-1; //插入数据的前一个数的下标
+        /**
+         * 1. insertIndex >=0 确保数组不越界
+         * 2. insertValue>arr[insertIndex] 确保未找到插入位置
+         * 3. indexIndex 移动向前移动  一个一个往前比
+         */
+        while (insertIndex >= 0 && insertValue < arr[insertIndex]){
+            arr[insertIndex+1]=arr[insertIndex];
+            insertIndex--;
+        }
+        arr[insertIndex+1]=insertValue;
+        System.out.println( "第"+(insertIndex+1)+"轮插入排序推导："+ Arrays.toString(arr) );
+        
+        
+        //由此推到
+        int[] arrI={133,22,44,1};
+        InsertSort is= new InsertSort();
+        is.insertSort( arrI );
+    }
+    
+    public void insertSort(int[] arr){
+        int insertValue=0;
+        int insertIndex=0; //插入数据的前一个数的下标
+        for(int i=1;i<arr.length;i++){
+            insertValue=arr[i];
+            insertIndex=i-1; //插入数据的前一个数的下标
+            /**
+             * 1. insertIndex >=0 确保数组不越界
+             * 2. insertValue>arr[insertIndex] 确保未找到插入位置
+             * 3. indexIndex 移动向前移动  一个一个往前比
+             */
+            while (insertIndex >= 0 && insertValue < arr[insertIndex]){
+                arr[insertIndex+1]=arr[insertIndex];
+                insertIndex--;
+            }
+            arr[insertIndex+1]=insertValue;
+            System.out.println( "推导后: 第"+(insertIndex+1)+"轮插入排序："+ Arrays.toString(arr) );
+        }
+    }
+
+    /**
+     * 输出结果 --与预期相符
+     * 第0轮插入排序推导：[22, 133, 44, 1]
+     * 第1轮插入排序推导：[22, 44, 133, 1]
+     * 第0轮插入排序推导：[1, 22, 44, 133]
+     * 推导后: 第0轮插入排序：[22, 133, 44, 1]
+     * 推导后: 第1轮插入排序：[22, 44, 133, 1]
+     * 推导后: 第0轮插入排序：[1, 22, 44, 133]
+     */
+}
---
+++ src/main/java/com/tmx/stuckData/sortArithmetic/SelectSort.java
@@ -0,0 +1,133 @@
+package com.tmx.stuckData.sortArithmetic;
+
+import java.util.Arrays;
+
+/**
+ * @author mingxiong.tang
+ * @Date: 2020/1/6 14:22
+ */
+public class SelectSort {
+    //选择排序思路
+    //选定一个 假定第一个小，以此类推  找到第一个最小的数 交换位置 。
+    public static void main(String[] args) {
+        int[] arrs={123,22,3,55};
+        boolean flagTemp=false;
+        int i=0;
+        int minIndex=i;
+        int minValue=arrs[minIndex];
+
+        for(int j=0+i;j<arrs.length;j++){
+            if(minValue>arrs[j]){
+                flagTemp=true;
+                minIndex=j;              //重置最小序列
+                minValue=arrs[j];           //重置最小值
+            }
+        }
+        //交换
+        if(minIndex!=i && flagTemp){
+            arrs[minIndex]=arrs[i];
+            arrs[i]=minValue;
+        }
+        System.out.println( "第"+(i+1)+"轮选择排序输出："+ Arrays.toString(arrs) );
+
+        flagTemp=false;
+         i=0+1;
+         minIndex=i;
+         minValue=arrs[minIndex];
+
+        for(int j=0+i;j<arrs.length;j++){
+            if(minValue>arrs[j]){
+                flagTemp=true;
+                minIndex=j;              //重置最小序列
+                minValue=arrs[j];           //重置最小值
+            }
+        }
+        //交换
+        if(minIndex!=i &&  flagTemp){
+            arrs[minIndex]=arrs[i];
+            arrs[i]=minValue;
+        }
+        System.out.println( "第"+(i+1)+"轮选择排序输出："+ Arrays.toString(arrs) );
+
+        //第三轮
+        flagTemp=false;
+         i=0+1+1;
+         minIndex=i;
+         minValue=arrs[minIndex];
+
+        for(int j=0+i;j<arrs.length;j++){
+            if(minValue>arrs[j]){
+                flagTemp=true;
+                minIndex=j;              //重置最小序列
+                minValue=arrs[j];           //重置最小值
+            }
+        }
+        //交换
+        if(minIndex!=i &&  flagTemp){
+            arrs[minIndex]=arrs[i];
+            arrs[i]=minValue;
+        }
+        System.out.println( "第"+(i+1)+"轮选择排序输出："+ Arrays.toString(arrs) );
+
+//        flagTemp=false;
+//        i=0+1+1;
+//        minIndex=i;
+//        minValue=arrs[minIndex];
+//
+//        for(int j=0+i;j<arrs.length;j++){
+//            if(minValue>arrs[j]){
+//                flagTemp=true;
+//                minIndex=j;              //重置最小序列
+//                minValue=arrs[j];           //重置最小值
+//            }
+//        }
+//        //交换
+//        if(minIndex!=i &&  flagTemp){
+//            arrs[minIndex]=arrs[i];
+//            arrs[i]=minValue;
+//        }
+//        System.out.println( "第"+i+"轮选择排序输出："+ Arrays.toString(arrs) );
+        //抽成方法selectSort(arrs);
+        int[] arrsA={123,22,3,55};
+        SelectSort ss=new SelectSort();
+        ss.selectSort( arrsA );
+    }
+    public void selectSort(int[] arrsA){
+        //由此推导
+        boolean flagTemp=false;
+        int i=0;
+        int minIndex=i;
+        int minValue=arrsA[minIndex];
+
+        for(int m=0;m<arrsA.length-1;m++){
+            flagTemp=false;
+            i=m;
+            minIndex=i;
+            minValue=arrsA[minIndex];
+
+            for(int j=0+i;j<arrsA.length;j++){
+                if(minValue>arrsA[j]){
+                    flagTemp=true;
+                    minIndex=j;              //重置最小序列
+                    minValue=arrsA[j];           //重置最小值
+                }
+            }
+            //交换
+            if(minIndex!=i &&  flagTemp){
+                arrsA[minIndex]=arrsA[i];
+                arrsA[i]=minValue;
+            }
+            System.out.println( "第"+(i+1)+"轮选择排序输出："+ Arrays.toString(arrsA) );
+        }
+    }
+    /**
+     * 输出结果：完全一致
+     * 第1轮选择排序输出：[3, 22, 123, 55]
+     * 第2轮选择排序输出：[3, 22, 123, 55]
+     * 第3轮选择排序输出：[3, 22, 55, 123]
+     * //推导后
+     * 第1轮选择排序输出：[3, 22, 123, 55]
+     * 第2轮选择排序输出：[3, 22, 123, 55]
+     * 第3轮选择排序输出：[3, 22, 55, 123]
+     */
+}
