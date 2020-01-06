f65accf598d280e95b41da5adb1ab40efacf1f1a
alivekeeper <308771934@qq.com>
2020-01-06 14:05:43 +0800

    冒泡排序

--- src/main/java/com/tmx/stuckData/sortArithmetic/BubbleSort.java
+++ src/main/java/com/tmx/stuckData/sortArithmetic/BubbleSort.java
@@ -24,5 +24,39 @@ public class BubbleSort {
             }
         }
         System.out.println( "第"+(i+1)+"轮数据："+ Arrays.toString(arr) );
+
+        i=i+1;
+
+        for(int j=1+i;j<arr.length;j++){
+            if(arr[i]>arr[j]){
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        System.out.println( "第"+(i+1)+"轮数据："+ Arrays.toString(arr) );
+
+        i=i+1;
+
+        for(int j=1+i;j<arr.length;j++){
+            if(arr[i]>arr[j]){
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        System.out.println( "第"+(i+1)+"轮数据："+ Arrays.toString(arr) );
+        //由此推导出 冒泡排序算法 双重循环
+        int[] arrO={111,55,6,77};
+        for(int m=0;m<arrO.length-1;m++){
+            for(int j=1+m;j<arrO.length;j++){
+                if(arrO[m]>arrO[j]){
+                    int temp=arrO[j];
+                    arrO[j]=arrO[m];
+                    arrO[m]=temp;
+                }
+            }
+            System.out.println( "标准化第"+(m+1)+"轮数据："+ Arrays.toString(arrO) );
+        }
     }
 }
